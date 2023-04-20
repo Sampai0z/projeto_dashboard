b753db1f760ee2e4474b10c8a4f1ad0cf58838df
Vitor Sampaio <vitor-sampaio@outlook.com.br>
2023-04-20 01:15:37 +0100

    Merge branch 'main' of https://github.com/Sampai0z/projeto_dashboard

--- dashboard-api/app.js
+++ dashboard-api/app.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-=======
 // importação
->>>>>>> 797120bc4e188baf4329801c0ae89909c0fa597f
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -10,7 +7,7 @@ const cors = require("cors");
 const fileUpload = require("express-fileupload");
 
 // LISTAGEM DE ROTAS
-<<<<<<< HEAD
+
 const homeRouter = require("./routes/routes");
 
 // START NO EXPRESS
@@ -45,35 +42,3 @@ try {
 	console.log("Erro ao iniciar server...");
 	console.log(error);
 }
-=======
-const homeRouter = require("./routes/routes")
-
-// LIGA O EXPRESS
-const app = express();
-
-const config = require('./config/config')
-const {Sequelize} = require("sequelize")
-const conection = new Sequelize(config.development)
-
-// INICIALIZAÇÃO DOS MODELS
-
-// ASSOCIAÇÃO DOS MODELS
-
-// ROTA BASE DA API
-app.use(cors())
-app.use(bodyParser.urlencoded({exteended: false}))
-app.use(bodyParser.json)
-app.use(fileUpload())
-
-//ROTAS
-app.use("/api", homeRouter)
-
-try {
-  app.listen(3000,()=>{
-    console.log("A API está ouvindo em http://localhost:3000")  
-  })
-} catch(error){
-    console.log('Deu ruim')
-    console.log(error)
-}
->>>>>>> 797120bc4e188baf4329801c0ae89909c0fa597f
